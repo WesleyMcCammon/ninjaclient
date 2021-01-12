0ae2c6bf78d759679a1f3bc186e01879c672be56
Wesley McCammon <wesleymccammon@hotmail.com>
2021-01-12 13:39:07 -0600

    code cleanup

--- ng-app/src/app/shared/component/price-tick-currency/price-tick-currency.component.ts
+++ ng-app/src/app/shared/component/price-tick-currency/price-tick-currency.component.ts
@@ -7,17 +7,20 @@ import { FuturesValueService } from 'src/app/reference/service/futures-value.ser
   styleUrls: ['./price-tick-currency.component.css']
 })
 export class PriceTickCurrencyComponent implements OnInit {
-  @Input() label: string;
-  @Input() ticker: string;
-  @Input() price: number;
-  @Input() ticks: number;
+  @Input() label: string = '';
+  @Input() ticker: string = '';
+  @Input() price: number = 0;
+  @Input() ticks: number = 0;
+  @Input() step: number = 1;
   @Output() change: EventEmitter<number> = new EventEmitter();  
+  showTicks: boolean = false;
 
   private _diff: number = 0;
 
   constructor(public futuresValueService: FuturesValueService) { }
 
   ngOnInit(): void {
+    this.showTicks = this.ticker.length > 0;
     this._diff = this.ticks;
     console.log('================ ' + this.ticks + ' ' + this.price);
   }
